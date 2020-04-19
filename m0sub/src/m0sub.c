942d2a2a49ddff5c9fe622c1b4bb2c6fe60b0625
Andrea Bettati <andrea.bettati@studenti.unipr.it>
2020-04-20 00:13:44 +0200

    Update m0sub.c

--- m0sub/src/m0sub.c
+++ m0sub/src/m0sub.c
@@ -10,10 +10,96 @@
 
 #include "board.h"
 #include <cr_section_macros.h>
+#include <stdio.h>
+#include <string.h>
+#include "app_usbd_cfg.h"
+#include "cdc_vcom.h"
 
+/*****************************************************************************
+ * Private types/enumerations/variables
+ ****************************************************************************/
+static uint32_t irq_cnt;
+
+static USBD_HANDLE_T g_hUsb;
+static uint8_t g_rxBuff[256];
+
+/* Endpoint 0 patch that prevents nested NAK event processing */
+static uint32_t g_ep0RxBusy = 0;/* flag indicating whether EP0 OUT/RX buffer is busy. */
+static USB_EP_HANDLER_T g_Ep0BaseHdlr;	/* variable to store the pointer to base EP0 handler */
+
+/*****************************************************************************
+ * Public types/enumerations/variables
+ ****************************************************************************/
+
+const USBD_API_T *g_pUsbApi;
+
+/*****************************************************************************
+ * Private functions
+ ****************************************************************************/
+
+/* EP0_patch part of WORKAROUND for artf45032. */
+ErrorCode_t EP0_patch(USBD_HANDLE_T hUsb, void *data, uint32_t event)
+{
+	switch (event) {
+	case USB_EVT_OUT_NAK:
+		if (g_ep0RxBusy) {
+			/* we already queued the buffer so ignore this NAK event. */
+			return LPC_OK;
+		}
+		else {
+			/* Mark EP0_RX buffer as busy and allow base handler to queue the buffer. */
+			g_ep0RxBusy = 1;
+		}
+		break;
+
+	case USB_EVT_SETUP:	/* reset the flag when new setup sequence starts */
+	case USB_EVT_OUT:
+		/* we received the packet so clear the flag. */
+		g_ep0RxBusy = 0;
+		break;
+	}
+	return g_Ep0BaseHdlr(hUsb, data, event);
+}
 /*****************************************************************************
  * Public functions
  ****************************************************************************/
+
+/**
+ * @brief	Handle interrupt from USB
+ * @return	Nothing
+ */
+void USB_IRQHandler(void)
+{
+	USBD_API->hw->ISR(g_hUsb);
+}
+
+/* Find the address of interface descriptor for given class type. */
+USB_INTERFACE_DESCRIPTOR *find_IntfDesc(const uint8_t *pDesc, uint32_t intfClass)
+{
+	USB_COMMON_DESCRIPTOR *pD;
+	USB_INTERFACE_DESCRIPTOR *pIntfDesc = 0;
+	uint32_t next_desc_adr;
+
+	pD = (USB_COMMON_DESCRIPTOR *) pDesc;
+	next_desc_adr = (uint32_t) pDesc;
+
+	while (pD->bLength) {
+		/* is it interface descriptor */
+		if (pD->bDescriptorType == USB_INTERFACE_DESCRIPTOR_TYPE) {
+
+			pIntfDesc = (USB_INTERFACE_DESCRIPTOR *) pD;
+			/* did we find the right interface descriptor */
+			if (pIntfDesc->bInterfaceClass == intfClass) {
+				break;
+			}
+		}
+		pIntfDesc = 0;
+		next_desc_adr = (uint32_t) pD + pD->bLength;
+		pD = (USB_COMMON_DESCRIPTOR *) next_desc_adr;
+	}
+
+	return pIntfDesc;
+}
 
 /**
  * Wait for message from M4 core
@@ -22,7 +108,9 @@ void M4_IRQHandler(void)
 {
 	// Clear M4 Event
 	LPC_CREG->M4TXEVENT = 0;
-    Board_LED_Toggle(0);
+    Board_LED_Set(0, false);
+    irq_cnt++;
+    __SEV();
 }
 
 
@@ -34,6 +122,54 @@ int main(void) {
     // Set the LED to the state of "On"
     Board_LED_Set(0, false);
 
+    // Setup USB ---------------------------------------------------------
+
+	USBD_API_INIT_PARAM_T usb_param;
+	USB_CORE_DESCS_T desc;
+	ErrorCode_t ret = LPC_OK;
+	uint32_t prompt = 0, rdCnt = 0;
+	USB_CORE_CTRL_T *pCtrl;
+
+	/* enable clocks and pinmux */
+	USB_init_pin_clk();
+
+	/* Init USB API structure */
+	g_pUsbApi = (const USBD_API_T *) LPC_ROM_API->usbdApiBase;
+
+	/* initialize call back structures */
+	memset((void *) &usb_param, 0, sizeof(USBD_API_INIT_PARAM_T));
+	usb_param.usb_reg_base = LPC_USB_BASE;
+	usb_param.max_num_ep = 4;
+	usb_param.mem_base = USB_STACK_MEM_BASE;
+	usb_param.mem_size = USB_STACK_MEM_SIZE;
+
+	/* Set the USB descriptors */
+	desc.device_desc = (uint8_t *) USB_DeviceDescriptor;
+	desc.string_desc = (uint8_t *) USB_StringDescriptor;
+	desc.high_speed_desc = USB_HsConfigDescriptor;
+	desc.full_speed_desc = USB_FsConfigDescriptor;
+	desc.device_qualifier = (uint8_t *) USB_DeviceQualifier;
+
+	/* USB Initialization */
+	ret = USBD_API->hw->Init(&g_hUsb, &desc, &usb_param);
+	if (ret == LPC_OK) {
+
+		pCtrl = (USB_CORE_CTRL_T *) g_hUsb;	/* convert the handle to control structure */
+		g_Ep0BaseHdlr = pCtrl->ep_event_hdlr[0];/* retrieve the default EP0_OUT handler */
+		pCtrl->ep_event_hdlr[0] = EP0_patch;/* set our patch routine as EP0_OUT handler */
+
+		/* Init VCOM interface */
+		ret = vcom_init(g_hUsb, &desc, &usb_param);
+		if (ret == LPC_OK) {
+			/*  enable USB interrupts */
+			NVIC_EnableIRQ(LPC_USB_IRQ);
+			/* now connect */
+			USBD_API->hw->Connect(g_hUsb, 1);
+		}
+
+	}
+
+	// End Setup USB -----------------------------------------------------
 
     NVIC_EnableIRQ(M4_IRQn);
 
@@ -46,6 +182,18 @@ int main(void) {
         // stepping of tight while() loop
         __asm volatile ("nop");
         __WFI();
+
+		if ((vcom_connected() != 0) && (prompt == 0)) {
+			vcom_write("Hello World!!\r\n", 15);
+			prompt = 1;
+		}
+		/* If VCOM port is opened echo whatever we receive back to host. */
+		if (prompt) {
+			rdCnt = vcom_bread(&g_rxBuff[0], 256);
+			if (rdCnt) {
+				vcom_write(&irq_cnt, 4);
+			}
+		}
     }
     return 0 ;
 }
